3c27ea91f6d592f5bd877a67b856d9fde8a6bcf3
Aplicaciones Moviles <Aplicaciones Moviles@NC1367>
2019-10-21 21:48:37 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoCalculadora/ProyectoCalculadora/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ProyectoCalculadora.Services;
+
+namespace ProyectoCalculadora
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button boton = sender as Button;
+
+            Mantisa.Text = boton.Text;
+
+
+        }
+    }
+}
